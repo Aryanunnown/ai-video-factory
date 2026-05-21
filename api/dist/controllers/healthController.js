aa91b6c7b3f908b6128be1af3c179be532f8e207
Anmol Arya <anmolarya@Anmols-MacBook-Pro.local>
2026-05-21 16:26:09 +0530

    Remove api dist from tracking

--- api/dist/controllers/healthController.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getHealth = void 0;
-const healthService_1 = require("../services/healthService");
-const getHealth = (req, res) => {
-    const result = (0, healthService_1.checkHealth)();
-    res.status(200).json(result);
-};
-exports.getHealth = getHealth;
--- api/dist/services/healthService.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.checkHealth = void 0;
-const checkHealth = () => ({
-    status: "ok",
-    uptime: process.uptime(),
-    timestamp: new Date().toISOString(),
-});
-exports.checkHealth = checkHealth;
